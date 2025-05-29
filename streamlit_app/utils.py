6dd4d7d15362f5487bd331f637e3107ca80255de
Abbas-MH <abbas.mzehamadi@gmail.com>
2025-05-29 17:29:31 +0200

    get poster script

---
+++ streamlit_app/utils.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from pathlib import Path
+
+
+
+
+# Définir le répertoire de sortie
+OUTPUT_DIR = Path(__file__).resolve().parent.parent / "output"
+
+
+# Fonction pour charger les données avec cache
+@st.cache_data
+def load_parquet_data(file_name):
+    file_path = OUTPUT_DIR / file_name
+    return pd.read_parquet(file_path)
